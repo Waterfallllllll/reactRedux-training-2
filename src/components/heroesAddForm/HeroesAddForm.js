e2f99246c76875c763e5db85c5a18f0440a213bd
Ilya <ilieturcan424@gmail.com>
2026-04-02 16:22:58 +0300

    Implement logic for adding a hero to JSON.

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,5 +1,6 @@
-
-
+import { Formik } from "formik";
+import { useHttp } from "../../hooks/http.hook";
+import { v4 as uuidv4 } from "uuid";
 // Задача для этого компонента:
 // Реализовать создание нового героя с введенными данными. Он должен попадать
 // в общее состояние и отображаться в списке + фильтроваться
@@ -11,48 +12,85 @@
 // данных из фильтров
 
 const HeroesAddForm = () => {
-    return (
-        <form className="border p-4 shadow-lg rounded">
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
-                <input 
-                    required
-                    type="text" 
-                    name="name" 
-                    className="form-control" 
-                    id="name" 
-                    placeholder="Как меня зовут?"/>
-            </div>
+  const { request } = useHttp();
+  return (
+    <Formik
+      initialValues={{ name: "", text: "", element: "" }}
+      onSubmit={(values, { setSubmitting }) => {
+        request(
+          "http://localhost:3001/heroes",
+          "POST",
+          JSON.stringify({
+            id: uuidv4(),
+            name: values.name,
+            description: values.text,
+            element: values.element,
+          }),
+        );
+        setSubmitting(false);
+      }}
+    >
+      {({ values, handleChange, isSubmitting, handleSubmit }) => (
+        <form onSubmit={handleSubmit} className="border p-4 shadow-lg rounded">
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label fs-4">
+              Имя нового героя
+            </label>
+            <input
+              required
+              type="text"
+              name="name"
+              className="form-control"
+              id="name"
+              placeholder="Как меня зовут?"
+              onChange={handleChange}
+              value={values.name}
+            />
+          </div>
 
-            <div className="mb-3">
-                <label htmlFor="text" className="form-label fs-4">Описание</label>
-                <textarea
-                    required
-                    name="text" 
-                    className="form-control" 
-                    id="text" 
-                    placeholder="Что я умею?"
-                    style={{"height": '130px'}}/>
-            </div>
+          <div className="mb-3">
+            <label htmlFor="text" className="form-label fs-4">
+              Описание
+            </label>
+            <textarea
+              required
+              name="text"
+              className="form-control"
+              id="text"
+              placeholder="Что я умею?"
+              style={{ height: "130px" }}
+              onChange={handleChange}
+              value={values.text}
+            />
+          </div>
 
-            <div className="mb-3">
-                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
-                <select 
-                    required
-                    className="form-select" 
-                    id="element" 
-                    name="element">
-                    <option >Я владею элементом...</option>
-                    <option value="fire">Огонь</option>
-                    <option value="water">Вода</option>
-                    <option value="wind">Ветер</option>
-                    <option value="earth">Земля</option>
-                </select>
-            </div>
+          <div className="mb-3">
+            <label htmlFor="element" className="form-label">
+              Выбрать элемент героя
+            </label>
+            <select
+              required
+              className="form-select"
+              id="element"
+              name="element"
+              onChange={handleChange}
+              value={values.element}
+            >
+              <option>Я владею элементом...</option>
+              <option value="fire">Огонь</option>
+              <option value="water">Вода</option>
+              <option value="wind">Ветер</option>
+              <option value="earth">Земля</option>
+            </select>
+          </div>
 
-            <button type="submit" className="btn btn-primary">Создать</button>
+          <button disabled={isSubmitting} type="submit" className="btn btn-primary">
+            Создать
+          </button>
         </form>
-    )
-}
+      )}
+    </Formik>
+  );
+};
 
 export default HeroesAddForm;
