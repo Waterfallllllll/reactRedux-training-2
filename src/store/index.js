0f0099e3a5ff7fce686e97add3aee7fe34db0364
Ilya <ilieturcan424@gmail.com>
2026-04-13 17:05:57 +0300

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,23 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const store = configureStore({ reducer: combineReducers({ heroes, filters }) });
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const oldDispatch = store.dispatch;
+
+    store.dispatch = (action) => {
+        if (typeof action === "string") {
+            return oldDispatch({
+                type: action
+            })
+        }
+        return oldDispatch(action);
+    }
+    return store;
+}
+
+const store = configureStore({ reducer: combineReducers({ heroes, filters }), enhancers: (getDefaultEnhancers) =>
+    getDefaultEnhancers().concat(enhancer),
+ });
 
 export default store;
